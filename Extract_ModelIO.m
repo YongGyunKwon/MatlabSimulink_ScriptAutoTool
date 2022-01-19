b6000bacd29f147916e24f9ddc0bc79cca62fb91
YongGyunKwon <ygkwon50@gmail.com>
2022-01-19 10:43:59 +0900

    Modify Extract_ModelIO.m Issu 입력

--- Extract_ModelIO.m
+++ Extract_ModelIO.m
@@ -1,13 +1,19 @@
 %Extract IO list in Model
 
+% Issue: Depth2 이상일 경우 필요없는 값들이 불러와서 해결필요
+
+
 clear;
 
-filename = 'D:\2_CodeBase\6_SimulinkTool\MatlabSimulink_ScriptAutoTool\Sample11.slx';
+filename = 'D:\2_CodeBase\6_SimulinkTool\MatlabSimulink_ScriptAutoTool\Sample.slx';
+Depth = 1;
+
+
 
 open_system(filename);
 
-Inport = find_system('SearchDepth',1,'BlockType','Inport');
-Outport = find_system('SearchDepth',1,'BlockType','Outport');
+Inport = find_system('SearchDepth',Depth,'BlockType','Inport');
+Outport = find_system('SearchDepth',Depth,'BlockType','Outport');
 
 
 Inport_Info = [Inport,get_param(Inport,'Name'),get_param(Inport,'Port')];
@@ -15,3 +21,4 @@ Outport_Info = [Outport,get_param(Outport,'Name'),get_param(Outport,'Port')];
 
 
 close_system();
+
