9c6a8f40a9eda4a2bc92ce73d21135916ed8afce
YongGyunKwon <ygkwon50@gmail.com>
2022-03-14 16:20:38 +0900

    Add ModiySigPara

---
+++ ModifyModel/Get_Para_Info.m
@@ -0,0 +1,28 @@
+
+parafilepath = 'D:\2_CodeBase\6_SimulinkTool\MatlabSimulink_ScriptAutoTool\ModifyModel\Sample_v_0_1_Parameter.m';
+
+Parameter_Info=Extract_ParameterInfo(parafilepath);
+
+function Parameter_Info=Extract_ParameterInfo(parafilepath)
+    Parameter_Import=importdata(parafilepath);
+    Scan_Index=1;
+
+    for PI_Index=1:size(Parameter_Import,1)
+        Parameter_Format="%s = %s ;";
+        Parameter_Scan_temp=textscan(string(Parameter_Import(PI_Index)),Parameter_Format);
+        
+        
+        if contains(string(Parameter_Scan_temp(1)),'%')==0
+            Parameter_Info(Scan_Index).ParameterName=string(Parameter_Scan_temp(1));
+    
+            Parameter_Scan_temper=split(string(Parameter_Scan_temp(2)),';');
+    
+            Parameter_Info(Scan_Index).Value=Parameter_Scan_temper(1);
+    
+            Scan_Index=Scan_Index+1;
+    
+        end
+    
+    end
+    
+end
---
+++ ModifyModel/Sample_v_0_1_Parameter.m
@@ -0,0 +1,7 @@
+KeyOff = 0;
+KeyOn = 1;
+
+
+%%Function Parameter
+One = 1;
+Two = 2;
